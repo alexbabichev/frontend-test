98bb3aef1a5284cfd636e36f72fea31f9b9be1b0
Jonathan Zelba <johnyzelba@gmail.com>
2019-09-17 09:50:01 +0300

    files

--- src/app/models/photo.ts
+++
@@ -1,9 +0,0 @@
-export class Photo {		
-	constructor(
-	  public search_tag?: string,
-		public url_q?: string,
-		public ownerName?: string,
-		public dateUploaded?: string,
-		public dateTaken?: string,
-		public numberOfViews?: number){}	  
-}
